775773010d36e925ad8e04ee7992b3fc1c139667
Evan Furniss <evanfurniss@gmail.com>
2021-02-25 19:15:28 -0600

    Added syntax back to html files

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workouts";
-mongoose.connect(MONGODB_URI,{ useNewUrlParser:true, useCreateIndex: true });
+mongoose.connect(MONGODB_URI,{ useNewUrlParser:true});
+// , useCreateIndex: true 
 
 app.use(require("./routes/db.js"))
 app.use(require("./routes/html.js"))
