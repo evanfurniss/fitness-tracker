29b6e18589812b216dde6da583418dbd83e415bb
Evan Furniss <evanfurniss@gmail.com>
2021-03-19 10:10:15 -0500

    Changed LinkedIn URL, changing here

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.static("public"));
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workouts";
 mongoose.connect(MONGODB_URI,{ useNewUrlParser:true});
-// , useCreateIndex: true 
 
 app.use(require("./routes/db.js"))
 app.use(require("./routes/html.js"))
